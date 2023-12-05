1bba9fc5d5e06782c9ddae7a6230cd5143d854c9
Mburu Rionge <victormburu3@gmail.com>
2023-12-04 18:41:02 -0800

    How to use lists as stacks and queues,  tuples, packing tuples, sequence

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,15 @@
+#include <object.h>
+#include <python.h>
+#include <listobject.h>
+
+void print_python_list_info(PyObject *p);
+{
+	long int size = PyList_Size(p);
+	int i;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[8] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
