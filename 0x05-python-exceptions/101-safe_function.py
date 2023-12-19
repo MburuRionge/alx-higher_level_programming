d02435bc85741d1880a00f531f36804e3232eecb
Mburu Rionge <victormburu3@gmail.com>
2023-12-19 06:42:23 -0800

    python exceptions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+from __future__ import print_function
+
+
+def safe_function(fct, *args):
+    try:
+        res = fct(*args)
+    except Exception as e:
+        print("Exception: {}".format(e), file=sys.stderr)
+        return None
+    else:
+        return res
