7b16c616a4588f078a33db5db2d212f6665ea0e4
Mburu Rionge <victormburu3@gmail.com>
2023-12-19 16:30:13 -0800

    Square that defines a square by: (based on 5-square.py)

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -29,15 +29,15 @@ class Square:
 
     @property
     def position(self):
-    """Get/set the current position of the aquare."""
-    return self.__position
+        """Get/set the current position of the aquare."""
+        return self.__position
 
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or len(value) != 2 or
-            not all(isinstance(num, int) for num in value) or
-            not all(num >= 0 for num in value)):
-        raise TypeError("position must be a tuple of 2 positive integers")
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
@@ -46,7 +46,7 @@ class Square:
 
     def my_print(self):
         """Print the square with the # character."""
-        if self.__size = 0:
+        if self.__size == 0:
             print("")
             return
 
