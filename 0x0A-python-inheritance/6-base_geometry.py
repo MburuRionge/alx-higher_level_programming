7fdfebf7d76d065a93ca2667568e895e7b8a7981
Mburu Rionge <victormburu3@gmail.com>
2024-01-10 16:16:05 -0800

    basegeomery class

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,6 +3,7 @@
 
 
 class BaseGeometry:
+    """represents base geometry class"""
     def area(self):
         """method to compute this area"""
         raise Exception("area() is not implemented")
