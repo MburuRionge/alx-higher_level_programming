2604911161bd0fbb1ca2e81afd91707cee7243f5
Mburu Rionge <victormburu3@gmail.com>
2024-01-10 16:24:04 -0800

    integer validator

--- 0x0A-python-inheritance/hold.py
+++ 0x0A-python-inheritance/hold.py
@@ -11,3 +11,11 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """Method which returns an area of rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """string represention method"""
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
