e25e0c31cdb93ed1ebd7ebb29337d5bc3f5a29ee
MburuRionge <victormburu3@gmail.com>
2024-03-17 19:59:44 -0700

    python mapping

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that lists states"""
+import MySQLdb
+
+
+def main(argv):
+    if len(argv) != 4:
+        print("Incorrect number of arguments")
+        return
+
+    db = MySQLdb.connect(host='localhost', user=argv[1],
+            passwd=argv[2], db=argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY id ASC')
+
+    for row in cur.fetchall():
+        print(row)
+
+    db.close()
+
+
+if __name__ == '__main__':
+    from sys import argv
+    main(argv)
---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+"""its a function that filters states"""
+import MySQLdb
+
+
+def main(argv):
+    if len(argv) != 4:
+        print('Incorrect number of arguments')
+        return
+
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
+                        db=argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER_BY id ASC')
+
+    for row in cur.fetchall():
+        if row[1][0] == "N":
+            print(row)
+
+    db.close()
+
+if __name__ == '__main__':
+    from sys import argv
+    main(argv)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+1
+
+
+
+
+
+"
