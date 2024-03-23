a95418f96dbb053b9e9114d9bddac27f22590439
Mburu Rionge <victormburu3@gmail.com>
2024-03-22 19:59:50 -0700

    updates

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -9,7 +9,12 @@ from sqlalchemy.orm import sessionmaker, relationship
 
 if __name__ == "__main__":
     engine = create_engine('mysqldb://{}@localhost:3306/{}'
-                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                            .format(
+                                sys.argv[1],
+                                sys.argv[2],
+                                sys.argv[3]
+                                )
+    )
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
