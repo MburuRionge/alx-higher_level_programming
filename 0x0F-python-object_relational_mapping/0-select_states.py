026babd2dd1d3f0f317f1eb2b234aa140eef27b7
Mburu Rionge <victormburu3@gmail.com>
2024-03-22 18:19:02 -0700

    updates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,23 +4,15 @@ import MySQLdb
 import sys
 
 
-def main(argv):
-    if len(argv) != 4:
-        print("Incorrect number of arguments")
-        return
-
+if __name__ == '__main__':
     db = MySQLdb.connect(host='localhost', user=argv[1],
                             passwd=argv[2], db=argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute('SELECT * FROM states ORDER BY id ASC')
+    cur.execute('SELECT * FROM states')
     row = cur.fetchall()
     for r in row:
         print(r)
 
     cur.close()
     db.close()
-
-
-if __name__ == '__main__':
-    main(argv)
