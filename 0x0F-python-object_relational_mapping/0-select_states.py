0ff8bcbe85fbbe444f4f6bdc937cf9d8191824b6
Mburu Rionge <victormburu3@gmail.com>
2024-03-22 18:29:58 -0700

    updates

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,6 +5,11 @@ import sys
 
 
 if __name__ == '__main__':
+    """check if number of arguments provided is enough."""
+    if len(sys.argv) != 4:
+        print("Incorrect number of arguments")
+        sys.exit(1)
+
     db = MySQLdb.connect(host='localhost', user=argv[1],
                             passwd=argv[2], db=argv[3], port=3306)
 
