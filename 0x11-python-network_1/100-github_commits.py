bf175dee350859522c48d6f4fed72066d788c763
Mburu Rionge <victormburu3@gmail.com>
2024-04-01 19:56:55 -0700

    updates

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,25 +1,19 @@
 #!/usr/bin/python3
 """ a script that takes 2 arguments and commits both to github. """
+import sys
 import requests
 
 
-def main(args):
-    repository_name = args[1]
-    owner_name = args[2]
-    url = 'https://developer.github.com/v3/repos/commits/'
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    r = requests.post(url, data=(repository_name, owner_name))
+    r = requests.get(url)
     commits = r.json()
     try:
-
         for i in range(10):
-        print("{}: {}".format(
-            commits[i].get("sha"),
-            commits[i].get("commit").get("author").get("name")))
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     except IndexError:
         pass
-
-
-if __name__ == '__main__':
-    import sys
-    main(sys.argv)
